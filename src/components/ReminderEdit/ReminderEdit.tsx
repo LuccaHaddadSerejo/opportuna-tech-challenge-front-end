43795d2b346f696a65b53fe00b9648037dd017ad
LuccaHaddadSerejo <hslucca@gmail.com>
2024-04-08 07:56:42 -0300

    feature (reminder edit): Added the logic to edit a reminder

--- src/components/ReminderEdit/ReminderEdit.tsx
+++ src/components/ReminderEdit/ReminderEdit.tsx
@@ -1,46 +1,73 @@
-import {
-  useState,
-  // useContext
-} from "react";
+import { useContext, useState } from "react";
 import "./ReminderEdit.css";
+import { UpdateReminder, schema } from "./schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { RemindersContext } from "../../contexts/RemindersContext";
+import { CalendarContext } from "../../contexts/CalendarContext";
 
-function ReminderEdit() {
+interface ReminderEditProps {
+  id: number;
+}
+
+function ReminderEdit({ id }: ReminderEditProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UpdateReminder>({
+    resolver: zodResolver(schema),
+  });
+
+  const { updateReminder } = useContext(RemindersContext);
+
+  const { selectedDate } = useContext(CalendarContext);
+
   const [reminderEditPopUp, setReminderEditPopUp] = useState(false);
+
+  const onSubmit = (data: UpdateReminder) => {
+    updateReminder(id, data, selectedDate.date);
+    setReminderEditPopUp(false);
+  };
 
   return (
     <>
       <button
         className="ReminderEditButton"
-        onClick={() => setReminderEditPopUp(true)}
-      >
+        onClick={() => setReminderEditPopUp(true)}>
         Edit
       </button>
       {reminderEditPopUp && (
         <div className="OutsideContainer">
           <div className="EditReminderPopUp">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <input
+                {...register("title")}
                 type="text"
                 name="title"
                 maxLength={15}
-                required
                 placeholder="Reminder Title"
               />
+              {errors.title && <span>{errors.title.message}</span>}
               <textarea
+                {...register("description")}
                 name="description"
                 maxLength={30}
-                required
                 placeholder="Reminder Description"
               />
-              <input type="time" name="time" required />
-              <input type="date" name="date" required />
+              {errors.description && <span>{errors.description.message}</span>}
+              <input {...register("time")} type="time" name="time" />
+              {errors.time && <span>{errors.time.message}</span>}
+              <input {...register("date")} type="date" name="date" />
+              {errors.date && <span>{errors.date.message}</span>}
               <input
+                {...register("city")}
                 type="text"
                 name="city"
                 maxLength={15}
-                required
                 placeholder="City"
               />
+              {errors.city && <span>{errors.city.message}</span>}
               <div className="ColorPicker">
                 {["blue", "red", "green", "yellow", "purple", "orange"].map(
                   (color) => (
@@ -54,26 +81,25 @@ function ReminderEdit() {
                         borderRadius: "50%",
                         cursor: "pointer",
                         backgroundColor: color,
-                      }}
-                    >
+                      }}>
                       <input
+                        {...register("color")}
                         type="radio"
                         name="color"
                         value={color}
-                        required
                         style={{ display: "none" }}
                       />
                     </label>
                   )
                 )}
               </div>
+              {errors.color && <span>{errors.color.message}</span>}
               <button type="submit">Confirm</button>
               <button
                 type="button"
                 onClick={() => {
                   setReminderEditPopUp(false);
-                }}
-              >
+                }}>
                 Cancel
               </button>
             </form>
